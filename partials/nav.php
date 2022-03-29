01ca1b652ac4a2295a34d9164dc53fa2fbc47df4
Steven <16929888+stevenkyritsis@users.noreply.github.com>
2022-03-29 11:18:06 -0400

    roles

---
+++ partials/nav.php
@@ -0,0 +1,44 @@
+<?php
+require_once(__DIR__ . "/../lib/functions.php");
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
+
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "$BASE_PATH",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}
+session_start();
+
+
+?>
+<!-- include css and js files -->
+<link rel="stylesheet" href="styles.css">
+<script src="helpers.js"></script>
+<nav>
+    <ul>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
+</nav>
